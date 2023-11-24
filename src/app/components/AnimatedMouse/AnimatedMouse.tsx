a6026378bec2f5f61260d5f6629b3744c3289562
Ignacio Kruchowski <ignakruchowski@gmail.com>
2023-11-24 16:06:31 -0300

    style: add some changes to styles in header

--- src/app/components/AnimatedMouse/AnimatedMouse.tsx
+++ src/app/components/AnimatedMouse/AnimatedMouse.tsx
@@ -1,7 +1,7 @@
 function AnimatedMouse() {
   return (
-    <div className="w-6 h-10 border-white border-2 rounded-full">
-      <div className="bg-white w-1 h-1 rounded-full mx-auto my-1 animate-[fingerScroll_1s_ease-in-out_infinite]"></div>
+    <div className="w-6 h-10 border-black border-2 rounded-full">
+      <div className="bg-black w-1 h-1 rounded-full mx-auto my-1 animate-[fingerScroll_1s_ease-in-out_infinite]"></div>
     </div>
   );
 }
--- src/app/components/HomeSection/HomeSection.tsx
+++ src/app/components/HomeSection/HomeSection.tsx
@@ -4,20 +4,20 @@ import profileImage from '../../../../public/profile.png';
 
 function HomeSection() {
   return (
-    <div className="bg-blue-500 h-screen grid grid-rows-3 xxs:p-3 md:grid-rows-5 xl:p-5 2xl:p-8">
-      <div className="row-span-2 flex flex-col justify-evenly md:flex-row md:row-span-3 md:justify-between">
-        <div className="p-5 s:w-3/4 lg:w-2/4">
-          <h2 className="text-5xl uppercase break-words sm:text-6xl lg:text-7xl xl:text-8xl 3xl:text-9xl">Frontend Developer</h2>
-        </div>
-        <div className="max-h-80 w-full md:max-w-xs 2xl:max-w-md 2xl:max-h-full">
+    <div className="bg-yellow-400 h-screen grid grid-rows-3 p-2 xxs:p-3 md:grid-rows-5 xl:p-5 2xl:p-8">
+      <div className="row-span-2 flex flex-col gap-5 md:flex-row md:row-span-3 md:justify-between">
+        <h2 className="text-5xl font-medium uppercase break-words sm:text-6xl s:w-3/4 lg:w-2/4 lg:text-7xl xl:text-8xl 3xl:text-9xl">Frontend Developer</h2>
+        <div className="max-h-96 w-full md:max-w-xs 2xl:max-w-sm 2xl:max-h-full">
           <Image src={profileImage} alt="Profile picture" className="object-top object-cover max-h-full w-full" />
         </div>
       </div>
-      <div className="row-span-2 flex flex-col-reverse justify-evenly items-center sm:flex-row sm:justify-between md:items-end">
-        <div className="sm:p-5">
+      <div className="row-span-2 pb-5 flex justify-between items-end sm:flex-row sm:justify-between">
+        <div className="p-2 sm:p-5">
           <AnimatedMouse />
         </div>
-        <h1 className="text-center uppercase text-4xl xs:text-5xl s:w-96 sm:text-right break-words sm:text-6xl sm:w-4/6 lg:text-7xl xl:text-8xl 2xl:w-6/12 3xl:text-9xl">Ignacio Kruchowski</h1>
+        <h1 className="text-right w-3/4 font-medium uppercase text-4xl xs:text-5xl s:w-96 sm:text-right break-words sm:text-6xl sm:w-4/6 lg:text-7xl xl:text-8xl 2xl:w-6/12 3xl:text-9xl">
+          Ignacio Kruchowski
+        </h1>
       </div>
     </div>
   );
