b8793ea432af61dad0be733cdf0b4f6e3e7818b5
Ignacio Kruchowski <ignakruchowski@gmail.com>
2023-12-06 15:37:11 -0300

    style: change size of titles in each section

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -6,14 +6,14 @@ function Header() {
   return (
     <div className="h-full grid grid-rows-6 md:grid-rows-5">
       <div className="flex flex-col gap-5 row-span-4 md:flex-row md:row-span-3 md:justify-between">
-        <h2 className="text-5xl font-medium uppercase break-words sm:text-6xl s:w-3/4 lg:w-2/4 lg:text-7xl xl:text-8xl 3xl:text-9xl">Frontend Developer</h2>
+        <h2 className="text-5xl font-medium uppercase break-words sm:text-6xl s:w-3/4 lg:w-2/4 lg:text-7xl xl:text-8xl 2xl:text-9xl">Frontend Developer</h2>
         <div className="w-full overflow-hidden md:max-w-xs md:max-h-72 xl:max-w-sm xl:max-h-80">
           <Image src={profileImage} alt="Profile picture" className="w-full max-h-full object-top object-cover" />
         </div>
       </div>
       <div className="flex justify-between items-end row-span-2 sm:flex-row sm:justify-between">
         <AnimatedMouse />
-        <h1 className="w-3/4 text-3xl text-right font-medium uppercase xs:text-4xl s:w-96 sm:text-right break-words sm:text-6xl sm:w-4/6 lg:text-7xl xl:text-8xl 2xl:w-6/12 3xl:text-9xl">
+        <h1 className="w-3/4 text-3xl text-right font-medium uppercase xs:text-4xl s:w-96 sm:text-right break-words sm:text-6xl sm:w-4/6 lg:text-7xl xl:text-8xl 2xl:w-8/12 2xl:text-9xl">
           Ignacio Kruchowski
         </h1>
       </div>
--- src/app/components/Title/Title.tsx
+++ src/app/components/Title/Title.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 function Title({ content }: Props) {
   return (
-    <h3 className="xxs:py-7 text-bold text-center mb-8 lg:mb-10 lg:text-left 2xl:mb-14 font-medium uppercase text-4xl xxs:text-5xl sm:text-6xl md:text-6xl lg:text-7xl xl:text-8xl 3xl:text-9xl">
+    <h3 className="xxs:py-7 text-bold text-center mb-8 lg:mb-10 lg:text-left 2xl:mb-14 font-medium uppercase text-4xl xxs:text-5xl sm:text-6xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl">
       {content}
     </h3>
   );
