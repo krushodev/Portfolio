97996018689e5ac87077c153894a4b62a6785a4a
krushodev <ignakruchowski@gmail.com>
2026-01-23 21:30:40 -0300

    fix: resolve responsive in first section

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -6,15 +6,17 @@ function Header() {
   return (
     <div className="h-full grid grid-rows-6 md:grid-rows-5">
       <div className="flex flex-col gap-5 row-span-4 md:flex-row md:row-span-3 md:justify-between">
-        <h2 className="text-5xl font-medium uppercase break-words sm:text-6xl s:w-3/4 lg:w-2/4 lg:text-7xl xl:text-8xl 2xl:text-9xl">Fullstack Developer</h2>
+        <h2 className="text-4xl font-medium uppercase break-words sm:text-5xl s:w-3/4 lg:w-2/4 lg:text-7xl xl:text-8xl 2xl:text-9xl">Fullstack Developer</h2>
         <div className="w-full max-h-80 md:max-w-xs md:max-h-72 xl:max-w-sm xl:max-h-80 z-10">
           <Image src={profileImage} alt="Profile picture" width={512} height={512} className="w-full max-h-full object-center object-cover brightness-95 contrast-105 rounded-md saturate-150" />
         </div>
       </div>
       <div className="flex justify-between items-end row-span-2 sm:flex-row sm:justify-between">
         <ScrollDownIndicator />
-        <h1 className="w-3/4 text-3xl text-right font-medium uppercase xs:text-4xl s:w-96 sm:text-right break-words sm:text-6xl sm:w-4/6 lg:text-7xl xl:text-8xl 2xl:w-8/12 2xl:text-9xl">
-          Ignacio Kruchowski
+        <h1 className="w-3/4 text-2xl text-right font-medium uppercase xs:text-3xl s:w-96 sm:text-right sm:text-4xl sm:w-4/6 lg:text-6xl xl:text-7xl 2xl:w-8/12 2xl:text-8xl">
+          Ignacio
+          <br />
+          Kruchowski
         </h1>
       </div>
     </div>
